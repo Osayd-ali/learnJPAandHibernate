aad54d22fdd276f64dcae72784e89ac319e93cea
Mir Osayd Ali <mali123@depaul.edu>
2025-08-25 01:58:49 -0500

    Modified method to create object of Course class

--- src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learn_jpa_and_hibernate.course.jdbc;
 
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,6 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
     }
 }
