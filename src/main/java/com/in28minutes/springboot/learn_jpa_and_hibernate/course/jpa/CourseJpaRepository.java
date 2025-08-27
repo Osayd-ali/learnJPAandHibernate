d222f36a2badcc79d4a2a8b3518b64a234884d13
Mir Osayd Ali <mali123@depaul.edu>
2025-08-26 23:01:32 -0500

    Introduced Course Jpa Repository file

---
+++ src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,8 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate.course.jpa;
+
+import org.springframework.stereotype.Repository;
+
+@Repository // To tell Spring that this is a repository class and it talks to the database.
+public class CourseJpaRepository {
+
+}
