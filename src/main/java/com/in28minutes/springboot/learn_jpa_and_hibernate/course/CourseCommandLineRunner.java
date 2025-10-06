c905f465720af4d29b1d8139689c2c02b8c42c29
Mir Osayd Ali <mali123@depaul.edu>
2025-10-05 23:47:14 -0500

    Introducing SpringDataJpa

--- src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.learn_jpa_and_hibernate.course;
 
 import com.in28minutes.springboot.learn_jpa_and_hibernate.course.jdbc.CourseJdbcRepository;
 import com.in28minutes.springboot.learn_jpa_and_hibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -16,18 +17,22 @@ public class CourseCommandLineRunner implements CommandLineRunner {
     //@Autowired
     //private CourseJdbcRepository repository;
 
+   // @Autowired
+   // private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS Now!", "in28minutes"));
-        repository.insert(new Course(2, "Learn Azure Now!", "in28minutes"));
-        repository.insert(new Course(3, "Learn DevOps Now!", "in28minutes"));
-        repository.insert(new Course(4, "Learn Amazon Now!", "in28minutes"));
-        repository.deleteById(2);
-        System.out.println(repository.findById(1));
-        System.out.println(repository.findById(3));
-
+        repository.save(new Course(1, "Learn AWS Now!", "in28minutes"));
+        repository.save(new Course(2, "Learn Azure Now!", "in28minutes"));
+        repository.save(new Course(3, "Learn DevOps Now!", "in28minutes"));
+        repository.save(new Course(4, "Learn Amazon Now!", "in28minutes"));
+        repository.deleteById(2l);
+        System.out.println(repository.findById(1l));
+        System.out.println(repository.findById(3l));
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
     }
 }
